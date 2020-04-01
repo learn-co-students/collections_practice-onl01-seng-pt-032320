9f8916ce5b4eb20d5f3ed2224f3bb70a182983d7
Jashema Panapa <jcpanapa@gmail.com>
2020-04-01 13:44:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc(array)
+array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse 
+end
+
+def sort_array_char_count(array)
+   array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index] 
+  
+end
+
+def reverse_array(array)
+  array.reverse
+end 
