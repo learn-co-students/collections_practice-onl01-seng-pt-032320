6adde54615c7bcec76b78f5205ce67950ddf54c5
aurel fontaine <ajfame11@gmail.com>
2020-04-02 03:12:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b
+      0
+      elsif a > b
+      -1
+      elsif a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+   new_word = word.split ""
+    new_word[2] = "$"
+    new_array << new_word.join
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |strings, index|
+    if index == 1
+      strings
+    else
+      strings << "s"
+    end
+  end
+end
