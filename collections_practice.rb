b38b41c70f377e83159ec96ff6db04feda8dd35d
Evan Smith <professionalemail@zoho.com>
2020-04-03 22:02:27 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+# sort by ascending order
+def sort_array_asc(array)
+  array.sort
+end
 
+# sort by descending order
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+# sort by char count
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+# swap elements
+def swap_elements(array)
+  first = array.pop
+  second = array.pop
+  array << first
+  array << second
+end
+
+# reverse array
+def reverse_array(array)
+  array.reverse
+end
+
+# kesha maker
+def kesha_maker(array)
+  
+end
