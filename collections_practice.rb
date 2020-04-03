73e0b7f254cb5d132f2d9a28be8ff964849af9e7
Evan Smith <professionalemail@zoho.com>
2020-04-03 22:27:05 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,5 +44,32 @@ end
 
 # kesha maker
 def kesha_maker(array)
-  
+  new_array = []
+  array.each do |name|
+    kesha = name.split("")
+    kesha.delete_at(2)
+    kesha.insert(2,"$")
+    kesha = kesha.join
+    new_array << kesha
+  end
+  new_array
+end
+
+# find a
+def find_a(array)
+  array.select do |letter|
+    letter.start_with?("a")
+  end
+end
+
+# sum array
+def sum_array(array)
+  array.inject { |a,b| a + b }
+end
+
+# add s
+def add_s(array)
+  array.each_with_index.collect do |elm, idx|
+    idx != 1 ? elm << "s" : elm
+  end
 end
